a95922a199a895907236371f8fa407753622de6c
nmdwolf <nicolas.dewolf@ugent.be>
2022-11-17 22:06:37 +0100

    blog test 3

--- setup.js
+++ setup.js
@@ -4,3 +4,9 @@ function setup() {
   total = total - parseFloat(window.getComputedStyle(document.getElementById("content")).getPropertyValue('padding-top'));
   document.getElementById("content").style.height = total + "px";
 }
+
+function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
