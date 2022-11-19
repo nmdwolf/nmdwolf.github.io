d22018a403c90d458c93a341f4070d1c04543877
nmdwolf <nicolas.dewolf@ugent.be>
2022-11-19 23:55:43 +0100

    scroll fixed

--- setup.js
+++ setup.js
@@ -10,9 +10,18 @@ function setup() {
   });
   */
 
-  const top = document.getElementById("content").getBoundingClientRect().top;
-  const top2 = document.getElementById("final").getBoundingClientRect().bottom;
-  content.style.height = Math.abs(top2 - top) + "px";
+  MathJax.typeset();
+
+  if(!!document.getElementById("final")) {
+    const top = document.getElementById("content").getBoundingClientRect().top;
+    const top2 = document.getElementById("final").getBoundingClientRect().bottom;
+    content.style.height = Math.abs(top2 - top) + "px";
+  }
+  else {
+    const height = content.style.height;
+    content.style.height = "calc(100% - " + (document.getElementById("content").getBoundingClientRect().top + 10) + "px";
+    content.style.height = Math.max(parseFloat(height), parseFloat(content.style.height)) + "px";
+  }
 }
 
 function getRandomInt(min, max) {
