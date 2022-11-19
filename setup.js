b3b807fe226d75aa153ae7e7ef4a0494bf03df4e
nmdwolf <nicolas.dewolf@ugent.be>
2022-11-19 23:10:08 +0100

    scroll test

--- setup.js
+++ setup.js
@@ -1,20 +1,32 @@
 function setup() {
-  const content = document.getElementById("content");
-  var total = window.innerHeight;
 
-  total = total - content.getBoundingClientRect().top;
-  total = total - parseFloat(window.getComputedStyle(content).getPropertyValue('padding-top'));
-  content.style.height = total + "px";
-
-  const base_height = total;
-
+  /*
   document.addEventListener("scroll", (event) => {
-    content.style.height = (base_height + window.scrollY) + "px";
+    if (!isInViewport(document.getElementById("final"))) {
+      const top = document.getElementById("content").getBoundingClientRect().top;
+      const top2 = document.getElementById("final").getBoundingClientRect().bottom;
+      //content.style.height = (top2 - top) + "px";
+    }
   });
+  */
+
+  const top = document.getElementById("content").getBoundingClientRect().top;
+  const top2 = document.getElementById("final").getBoundingClientRect().bottom;
+  content.style.height = Math.abs(top2 - top) + "px";
 }
 
 function getRandomInt(min, max) {
     min = Math.ceil(min);
     max = Math.floor(max);
     return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function isInViewport(element) {
+    const rect = element.getBoundingClientRect();
+    return (
+        //rect.top >= 0 &&
+        //rect.left >= 0 &&
+        rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) //&&
+        //rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+    );
 }
